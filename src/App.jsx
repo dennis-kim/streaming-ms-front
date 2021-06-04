34042079c6aeb9081e8e11a5c3734d7038b65e75
김민섭 <>
2021-06-05 00:22:34 +0900

    기본 리스트 구조 임시 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import React from "react";
-import {useMediaQuery} from 'react-responsive'
 import TopBar from "./container/TopBar/TopBar";
+import ContentsList from './container/ContentsList/ContentsList';
 import './style.css'
-
-const Desktop = ({ children }) => {
-  const isDesktop = useMediaQuery({ minWidth: 992 })
-  return isDesktop ? children : null
-}
-const Tablet = ({ children }) => {
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 })
-  return isTablet ? children : null
-}
-const Mobile = ({ children }) => {
-  const isMobile = useMediaQuery({ maxWidth: 767 })
-  return isMobile ? children : null
-}
 
 const App = () => {
  
@@ -22,11 +9,7 @@ const App = () => {
     <>
       <div>
         <TopBar />
-        <Desktop>
-          Desktop or laptop
-        </Desktop>
-        <Tablet>Tablet</Tablet>
-        <Mobile>Mobile</Mobile>
+        <ContentsList/>
         
       </div>
     </>
