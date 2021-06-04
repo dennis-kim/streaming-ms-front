4d2443fb0a17ceed760cb6113fde3d503a64de22
김민섭 <>
2021-06-05 01:06:35 +0900

    컨텐츠 아이템 초기 커밋

---
+++ src/component/ContentsItem/ContentsItem.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import './style.css'
+import sampleImg from './images/p_images/sub_gallery_01.jpg'
+
+const ContentsItem = ({  }) => {
+    
+    
+        return (
+            <div>
+                <img src={sampleImg} alt=""/>
+            </div>
+        );
+    
+}
+export default ContentsItem;
--- src/container/ContentsList/ContentsList.jsx
+++ src/container/ContentsList/ContentsList.jsx
@@ -1,21 +1,6 @@
 import React from "react";
 import './style.css'
-
-
-
-import sub_gallery_01 from './images/p_images/sub_gallery_01.jpg'
-import sub_gallery_02 from './images/p_images/sub_gallery_02.jpg'
-import sub_gallery_03 from './images/p_images/sub_gallery_03.jpg'
-import sub_gallery_04 from './images/p_images/sub_gallery_04.jpg'
-import sub_gallery_05 from './images/p_images/sub_gallery_05.jpg'
-import sub_gallery_06 from './images/p_images/sub_gallery_06.jpg'
-import sub_gallery_07 from './images/p_images/sub_gallery_07.jpg'
-import sub_gallery_08 from './images/p_images/sub_gallery_08.jpg'
-import sub_gallery_09 from './images/p_images/sub_gallery_09.jpg'
-import sub_gallery_10 from './images/p_images/sub_gallery_10.jpg'
-import sub_gallery_11 from './images/p_images/sub_gallery_11.jpg'
-import sub_gallery_12 from './images/p_images/sub_gallery_12.jpg'
-
+import ContentsItem from '../../component/ContentsItem/ContentsItem'
 
 const ContentsList = () => {
  
@@ -25,21 +10,23 @@ const ContentsList = () => {
                 <div>
                     <ul className="gallery_list">
                         <li>
-                            <div>
-                                <img src={sub_gallery_01} alt=""/>
-                            </div>
+                            <ContentsItem />
                         </li>
-                        <li><img src={sub_gallery_02} alt=""/></li>
-                        <li><img src={sub_gallery_03} alt=""/></li>
-                        <li><img src={sub_gallery_04} alt=""/></li>
-                        <li><img src={sub_gallery_05} alt=""/></li>
-                        <li><img src={sub_gallery_06} alt=""/></li>
-                        <li><img src={sub_gallery_07} alt=""/></li>
-                        <li><img src={sub_gallery_08} alt=""/></li>
-                        <li><img src={sub_gallery_09} alt=""/></li>
-                        <li><img src={sub_gallery_10} alt=""/></li>
-                        <li><img src={sub_gallery_11} alt=""/></li>
-                        <li><img src={sub_gallery_12} alt=""/></li>
+
+                        <li>
+                            <ContentsItem />
+                        </li>
+                        <li>
+                            <ContentsItem />
+                        </li>
+                        <li>
+                            <ContentsItem />
+                        </li>
+
+                        <li>
+                            <ContentsItem />
+                        </li>
+                        
                     </ul>
                 </div>
             </section>            
