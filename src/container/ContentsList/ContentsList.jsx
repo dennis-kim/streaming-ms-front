82275b92278a81b3d19a64205189e28ab302dfad
DESKTOP-2G64TMN\kim <>
2021-06-10 02:33:37 +0900

    무한 스크롤 테스트 https://godsenal.com/posts/React-Intersection-Observer%EB%A5%BC-%EC%82%AC%EC%9A%A9%ED%95%98%EC%97%AC-%EC%9D%B8%ED%94%BC%EB%8B%88%ED%8A%B8-%EC%8A%A4%ED%81%AC%EB%A1%A4-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0/

--- src/container/ContentsList/ContentsList.jsx
+++ src/container/ContentsList/ContentsList.jsx
@@ -4,6 +4,7 @@ import './style.css'
 import PropTypes from 'prop-types';
 import ContentsItem from '../../component/ContentsItem/ContentsItem'
 import API from '../../api'
+import useIntersect from "./useIntersect";
 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
@@ -11,26 +12,83 @@ function useQuery() {
 
 const ContentsList = ({match}) => {
     const query = useQuery();
+    const [page, setPage] = useState('1');
+    const [size, setSize] = useState('40');
+    const [sort, setSort] = useState('modify_date');
+    const [order, setOrder] = useState('desc');
+
     const [contents, setContents] = useState([]);
-    
-    useEffect(() => {
-        API.getContents(4, 'modify_date', 'desc')
+
+
+
+    const getContents = (baseId) => {
+        API.getContents(4, sort, order, page, size)
         .then((result) => {
             setContents(result)
+            console.log('api')
         })
         .catch((e) => {
-            console.log('error')
-        })  
+            console.log('err')
+        })
 
-        
+        setPage(page+1)
+    }
+    
+    
+    /* fake async fetch */
+    const fakeFetch = (delay = 1000) => new Promise(res => setTimeout(res, delay));
+    const [state, setState] = useState({ itemCount: 0, isLoading: false });
+    const fetchItems = async () => {
+        setState(prev => ({ ...prev, isLoading: true }));
+        await fakeFetch();
+        setState(prev => ({
+        itemCount: prev.itemCount + 10,
+        isLoading: false
+        }));
+    };
+
+
+
+
+
+
+
+    useEffect(() => {
+        getContents(4)
+        fetchItems();
     }, []);
+
+
+
+
+    const [_, setRef] = useIntersect(async (entry, observer) => {
+        observer.unobserve(entry.target);
+        await fetchItems();
+        observer.observe(entry.target);
+      }, {});
+    const { itemCount, isLoading } = state;
+    if (!itemCount) return null;
+
+
+
 
     if("/" === match.url){
         document.location.href = '/contents'
     }
+    
+
+    
+
+
+    const ListItem = ({ number }) => (
+        <div className="ListItem">
+          <span>{number}</span>
+        </div>
+      );
+
     return (
         <>
-            <section className="content_section">
+            {/* <section className="content_section">
                 <div>
                     <ul className="gallery_list">
                         {
@@ -44,7 +102,22 @@ const ContentsList = ({match}) => {
                         }
                     </ul>
                 </div>
-            </section>
+            </section> */}
+
+<div className="App">
+      {[...Array(itemCount)].map((_, i) => {
+        return (
+            // contents.length > 0 ?
+            // getContents(4, sort, order, page, size) : <></>
+            // console.log('aaa') : console.log('bbb')
+            <ListItem key={i} number={i} />
+        )
+        
+      })}
+      <div ref={setRef} className="Loading">
+        {isLoading && "Loading..."}
+      </div>
+    </div>
         </>
     );
 }
