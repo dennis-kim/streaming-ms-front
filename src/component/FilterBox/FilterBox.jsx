37cc27b6a9e01a1a8f6e77a02e698b4f9c02ba0a
김민섭 <>
2021-06-13 05:40:55 +0900

    필터박스-셀렉트박스 추가

--- src/component/FilterBox/FilterBox.jsx
+++ src/component/FilterBox/FilterBox.jsx
@@ -1,22 +1,20 @@
-import React from "react";
-import './style.css';
+import React, { Component } from 'react';
+import Select from 'react-select'
 
-const FilterBox = ({ }) => {
-        return (
-            <>
-                <div className="filterbox_container">
-                    aaaaa
-                    bbbbb
-                </div>
-            </>
-        );
-    
+
+const options = [
+    { value: 'chocolateㅁㅁㅁ', label: '영화' },
+    { value: 'strawberry', label: '예능' },
+    { value: 'vanilla', label: '드라마' }
+  ]
+
+class FilterBox extends Component {
+  render() {
+    return (
+        <div className="filterbox_container">
+            <Select options={options}/>
+        </div>
+    );
+  }
 }
-
-FilterBox.propType = {
-}
-
-FilterBox.defaultProps = {
-}
-
 export default FilterBox;
--- src/container/TopBar/TopBar.jsx
+++ src/container/TopBar/TopBar.jsx
@@ -7,7 +7,7 @@ import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 
 
 const TopBar = () => {
-    const [hideOfFilter, setHideOfFilter] = useState(true)
+    const [hideOfFilter, setHideOfFilter] = useState(false)
     
     const switchOfFilter = () => {
         setHideOfFilter(!hideOfFilter)
