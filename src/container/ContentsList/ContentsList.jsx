9fceca8e435e3c129c7e1d751c282ed9538ca3f3
김민섭 <>
2021-06-13 01:50:01 +0900

    intersect 리스트 구조 완료

--- src/container/ContentsList/ContentsList.jsx
+++ src/container/ContentsList/ContentsList.jsx
@@ -17,41 +17,45 @@ const ContentsList = ({match}) => {
     const [sort, setSort] = useState('modify_date');
     const [order, setOrder] = useState('desc');
 
-    const [contents, setContents] = useState([]);
+    // const [contents, setContents] = useState([]);
 
 
 
-    const getContents = (baseId) => {
-        API.getContents(4, sort, order, page, size)
-        .then((result) => {
-            setContents(result)
-            console.log('api')
-        })
-        .catch((e) => {
-            console.log('err')
-        })
+    // const getContents = (baseId) => {
+    //     API.getContents(4, sort, order, page, size)
+    //     .then((result) => {
+    //         setContents(result)
+    //         console.log('api')
+    //     })
+    //     .catch((e) => {
+    //         console.log('err')
+    //     })
 
-        setPage(page+1)
-    }
+    //     setPage(page+1)
+    // }
     
     const sample  = [
-        ['aaa'],
-        ['bbb'],
-        ['ccc'],
-        ['ddd'],
-        ['eee'],
-        ['fff'],
-        ['ggg'],
-        ['hhh']
+        {
+            contents_name: '승리호'
+        },
+        {
+            contents_name: '아이언맨'
+        },
+        {
+            contents_name: '어벤져스'
+        },
+        {
+            contents_name: '감자'
+        },
     ]
     
     /* fake async fetch */
-    const fakeFetch = (delay = 1000) => new Promise(res => setTimeout(res, delay));
+    const fakeFetch = (delay = 500) => new Promise(res => setTimeout(res, delay));
 
 
     const [intersectState, setIntersectState] = useState(
         { 
-            itemCount: sample,
+            contents: [],
             isLoading: false 
         }
     );
@@ -60,13 +64,10 @@ const ContentsList = ({match}) => {
         setIntersectState(prev => ({ ...prev, isLoading: true }));
         await fakeFetch();
 
-
-        // this.setState({ myArray: [...this.state.myArray, 'new value'] })
         setIntersectState(prev => ({
-        itemCount: itemCount.push(sample),
-        isLoading: false
+            contents: contents.push(...sample),
+            isLoading: false
         }));
-        // console.log('###', itemCount.length)
     };
 
     useEffect(() => {
@@ -80,8 +81,8 @@ const ContentsList = ({match}) => {
         observer.observe(entry.target);
       }, {});
 
-    const { itemCount, isLoading } = intersectState;
-    if (!itemCount) return null;
+    const { contents, isLoading } = intersectState;
+    if (!contents) return null;
 
     if("/" === match.url){
         document.location.href = '/contents'
@@ -92,17 +93,13 @@ const ContentsList = ({match}) => {
             <section className="content_section">
                 <div>
                     <ul className="gallery_list">
-
-
-                        {[...Array(itemCount)].map((_, i) => {
-                                return (
-                                    
-                                    <li key={i}>
-                                        <ContentsItem key={i} index={i} contentsName={i}/>
-                                    </li>
-                                )
-                                
-                            })}
+                        {[...Array(contents)].map((_, i) => {
+                            return (
+                                <li key={i}>
+                                    <ContentsItem key={i} index={i} contentsName={i}/>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
                 <div ref={setRef} className="Loading">
